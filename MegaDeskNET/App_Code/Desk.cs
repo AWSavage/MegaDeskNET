ba3ef139ab5c20109d8ab46d6cc236cd936403f9
tylerjamesmadsen <mad09001@byui.edu>
2018-06-20 09:52:36 -0600

    Added Desk and DeskQuote classes in App_Code folder.

---
+++ MegaDeskNET/App_Code/Desk.cs
@@ -0,0 +1,19 @@
+﻿namespace MegaDesk
+{
+    struct Desk
+    {
+        public enum DesktopSurfaceMaterial
+        {
+            Laminate,
+            Oak,
+            Rosewood,
+            Veneer,
+            Pine
+        }
+
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int NumberOfDrawers { get; set; }
+        public DesktopSurfaceMaterial SurfaceMaterial { get; set; }
+    }
+}
